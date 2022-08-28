560cb9354751048571981974baccb0a500c9a95b
Frankz <Frankz@PC-Frankz>
2022-08-27 20:58:11 -0500

    test en branch development

--- src/init/TestProyectoDisenoSoftware.java
+++ src/init/TestProyectoDisenoSoftware.java
@@ -16,7 +16,7 @@ public class TestProyectoDisenoSoftware {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Testeando commit a development branch");
+        System.out.println("Enviando a la rama development");
     }
     
 }
