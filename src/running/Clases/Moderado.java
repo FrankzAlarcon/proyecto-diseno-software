66a17bd90f2df246b7bee56e0a6dac2c65327475
Vicente0709 <78982050+Vicente0709@users.noreply.github.com>
2022-08-30 17:58:48 -0500

    GuiRunning

--- src/running/Clases/Moderado.java
+++ src/running/Clases/Moderado.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package running;
+package running.Clases;
 
 /**
  *
  * @author Stalin
  */
-public class RutinaRunning {
+public class Moderado {
     
 }
