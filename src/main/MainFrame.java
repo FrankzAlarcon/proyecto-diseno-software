c7cd801c1ffcce7f57a100b95e6a92b681c8dcac
Arcticguana <greenlantern0@hotmail.com>
2022-08-30 15:45:08 -0500

    GUI TrailRunning e Interfaz ReturnHandler

---
+++ src/main/MainFrame.java
@@ -0,0 +1,53 @@
+package main;
+
+import com.formdev.flatlaf.FlatDarkLaf;
+import com.formdev.flatlaf.FlatLightLaf;
+import trailrunning.TrailRunningPane;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
+    public static void main(String[] args) {
+
+        try {
+            UIManager.setLookAndFeel(new FlatLightLaf());
+        } catch (UnsupportedLookAndFeelException e) {
+            throw new RuntimeException(e);
+        }
+
+        MainFrame frame = new MainFrame();
+        frame.setMainPanel(new TrailRunningPane(frame));
+        frame.setVisible(true);
+    }
+    private JButton regresarBtn;
+    private ReturnHandler currentPane;
+    private JPanel mainPanel;
+    public MainFrame() {
+        regresarBtn = new JButton("Regresar");
+        mainPanel = new JPanel(new BorderLayout());
+        //mainPanel.add(regresarBtn);
+        setContentPane(mainPanel);
+
+
+        JPanel buttonPane = new JPanel();
+        regresarBtn.addActionListener(a -> {
+            currentPane.doReturnAction();
+        });
+
+        buttonPane.add(regresarBtn);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(500,500);
+
+        mainPanel.add(buttonPane, BorderLayout.SOUTH);
+
+    }
+
+    public void setMainPanel(ReturnHandler panel){
+        if(currentPane != null)
+            mainPanel.remove(currentPane.getReference());
+        currentPane = panel;
+        mainPanel.add(panel.getReference(), BorderLayout.CENTER);
+        mainPanel.revalidate();
+    }
+}
--- src/trailrunning/TrailRunningPane.java
+++ src/trailrunning/TrailRunningPane.java
@@ -1,6 +1,27 @@
 package trailrunning;
 
-import javax.swing.*;
+import main.MainFrame;
+import main.ReturnHandler;
 
-public class TrailRunningPane extends JPanel {
+import javax.swing.*;
+import java.awt.*;
+
+public class TrailRunningPane extends JPanel implements ReturnHandler {
+    private MainFrame mainFrame;
+    public TrailRunningPane(MainFrame mainFrame) {
+        this.mainFrame = mainFrame;
+        setOpaque(true);
+        setBackground(Color.RED);
+        add(new JLabel("Trailing 1"));
+    }
+
+    @Override
+    public void doReturnAction() {
+        mainFrame.setMainPanel(new PanelPrueba(mainFrame));
+    }
+
+    @Override
+    public JPanel getReference() {
+        return this;
+    }
 }
