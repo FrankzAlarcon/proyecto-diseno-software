b821c1e610f45f5142c7edf37a5b573cf59f7ffe
Anthony Simbaña <anthonymales200105@gmail.com>
2022-08-30 17:16:06 -0500

    Configuración dela clase SensorPresion (Observador).Padel

--- src/main/SensorPresion.java
+++ src/main/SensorPresion.java
@@ -10,6 +10,48 @@ package main;
  */
 public class SensorPresion extends Observador{
 
+    private float sensibilidad;
+    private int numeroGolpes;
+    private Presion presion;
+
+    public SensorPresion(float sensibilidad, int numeroGolpes, Presion presion) {
+        this.sensibilidad = sensibilidad;
+        this.numeroGolpes = numeroGolpes;
+        this.presion = presion;
+    }
+
+    public float getSensibilidad() {
+        return sensibilidad;
+    }
+
+    public void setSensibilidad(float sensibilidad) {
+        this.sensibilidad = sensibilidad;
+    }
+
+    public int getNumeroGolpes() {
+        return numeroGolpes;
+    }
+
+    public void setNumeroGolpes(int numeroGolpes) {
+        this.numeroGolpes = numeroGolpes;
+    }
+
+    public Presion getPresion() {
+        return presion;
+    }
+
+    public void setPresion(Presion presion) {
+        this.presion = presion;
+    }
+
+    public Observado getObservado() {
+        return observado;
+    }
+
+    public void setObservado(Observado observado) {
+        this.observado = observado;
+    }
+    
     @Override
     public void actualizar() {
         
