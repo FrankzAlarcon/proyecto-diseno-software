b808a22baa9f1c0d5573540b010643300432abb8
Andrés Ponce <andres.ponce@epn.edu.ec>
2022-08-30 16:15:46 -0500

    Init grupo Padel

---
+++ src/padel/ControladorPresion.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package padel;
+
+/**
+ *
+ * @author Grupo Padel: Jairo García, Santiago Gualotuña, Andrés Ponce, Anthony Simbaña
+ */
+public class ControladorPresion {
+    
+}
