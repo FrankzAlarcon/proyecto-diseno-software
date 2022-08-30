4f10a3fe7a774b2f64771f58bd436b05bbdba9c2
Joxxx69 <santiago.gualotuna@hotmail.com>
2022-08-30 16:58:01 -0500

    agregacion de variables privadas

--- src/main/Posicion.java
+++ src/main/Posicion.java
@@ -1,15 +1,51 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package main;
 
+import main.SensorUbicacion;
 /**
- *
  * @author Grupo Padel: Jairo García, Santiago Gualotuña, Andrés Ponce, Anthony Simbaña
  */
 public class Posicion extends Observado{
 
+    private float posX;
+    private float posY;
+ 
+    private SensorUbicacion sensor;
+
+    public Posicion(float posX, float posY, SensorUbicacion sensor) {
+        this.posX = posX;
+        this.posY = posY;
+        this.sensor = sensor;
+    }
+    
+    
+    
+
+    public float getPosX() {
+        return posX;
+    }
+
+    public void setPosX(float posX) {
+        this.posX = posX;
+    }
+
+    public float getPosY() {
+        return posY;
+    }
+
+    public void setPosY(float posY) {
+        this.posY = posY;
+    }
+
+    public SensorUbicacion getSensor() {
+        return sensor;
+    }
+
+    public void setSensor(SensorUbicacion sensor) {
+        this.sensor = sensor;
+    }
+    
+    
     @Override
     public void notificar() {
         
