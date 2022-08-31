87c5c3c0603fd42b101d43ea19249761fd1aca48
Freddyp666 <freddy-pat@hotmail.es>
2022-08-31 02:26:51 -0500

    Panel

--- src/cycling/CyclingPane.java
+++ src/cycling/CyclingPane.java
@@ -127,7 +127,7 @@ public class CyclingPane extends JPanel implements ReturnHandler{
 
     private void jToggleButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jToggleButton1ActionPerformed
         // TODO add your handling code here:
-        //mainFrame.setMainPanel(new RutinaInternaPanel(mainFrame));
+        mainFrame.setMainPanel(new RutinaInternaPanel(mainFrame));
     }//GEN-LAST:event_jToggleButton1ActionPerformed
 
 
