2a608aaac0cc4aaa751d24f35f1a4f79e8e22592
Andrés Ponce <andres.ponce@epn.edu.ec>
2022-09-03 16:02:41 -0500

    Implementación de disparadores para Raqueta

--- src/padel/Raqueta.java
+++ src/padel/Raqueta.java
@@ -58,25 +58,4 @@ public class Raqueta {
         this.controladorPosicion = null;
         this.controladorPresion = null;
     };
-    
-    public static void main(String[] args) {
-        Posicion posicion = new Posicion();
-        SensorPosicion sensorPosicion = new SensorPosicion();
-        ControladorPosicion controladorPosicion = new ControladorPosicion();
-        
-        posicion.setObservador(sensorPosicion);
-        sensorPosicion.setControlador(controladorPosicion);
-        controladorPosicion.setObservador(sensorPosicion);
-        
-        posicion.setPos(100, 200);
-        posicion.notificar();
-        
-        posicion.setPos(-500, +20);
-        posicion.notificar();
-        
-        posicion.setPos(10, 213);
-        posicion.notificar();
-        
-        System.out.println(controladorPosicion.getDistanciaRecorrida() + " metros.");
-    }
 }
